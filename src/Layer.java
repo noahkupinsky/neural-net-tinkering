bf5b5ffdf3e8dc4de8616a79cfe530660d806e2c
noahkupinsky <noah@kupinsky.com>
2023-03-02 18:04:59 -0500

    basic NN working

--- src/Layer.java
+++ src/Layer.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class Layer {
     //general class that handles matrix operation
     int nodes_from;
@@ -19,15 +21,15 @@ public class Layer {
     }
 
     private static double randomize_weight(){
-        return 1;
+        return Math.random() * 2 - 1;
     }
 
     private static double randomize_bias(){
-        return 1;
+        return Math.random() * 2 - 1;
     }
 
     public static double sigmoid(double x){
-        return 1 / (1 + Math.exp(-x));
+        return 1.0 / (1.0 + Math.exp(-x));
     }
 
     public static double sigmoid_derivative(double sigmoid_value){
@@ -40,8 +42,28 @@ public class Layer {
             outputs[to] = biases[to];
             for (int from = 0; from < nodes_from; from++)
                 outputs[to] += inputs[from] * weights[from][to];
+            outputs[to] = sigmoid(outputs[to]);
         }
         return outputs;
+    }
+
+    public double[] adjust_constants(double[] layer_derivatives, double[] prev_layer_outputs){
+        double[] prev_layer_derivatives = new double[nodes_from];
+
+        for (int to = 0; to < nodes_to; to++) {
+            biases[to] -= NeuralNet.LEARNING_RATE * layer_derivatives[to];
+            for (int from = 0; from < nodes_from; from++)
+                weights[from][to] -= NeuralNet.LEARNING_RATE * layer_derivatives[to] * prev_layer_outputs[from];
+        }
+
+        for (int from = 0; from < nodes_from; from++){
+            prev_layer_derivatives[from] = 0;
+            for (int to = 0; to < nodes_to; to++)
+                prev_layer_derivatives[from] += layer_derivatives[to] * weights[from][to];
+            prev_layer_derivatives[from] *= sigmoid_derivative(prev_layer_outputs[from]);
+        }
+
+        return prev_layer_derivatives;
     }
 }
 //each node has a bias and a weights coming from each node in the prev layer
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,38 @@
 public class Main {
+    static final int IO_SIZE = 5;
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        NeuralNet net = new NeuralNet(new int[] {IO_SIZE, 10, IO_SIZE});
+        for (int i = 0; i < 1000000; i++){
+            double[] input = generate_input();
+            double[] expected_output = generate_expected_output(input);
+            net.process_input(input, expected_output, (i > 1000000 - 10 || i < 11));
+        }
+    }
+
+    public static double[] generate_input(){
+        double[] input = new double[IO_SIZE];
+
+        for (int i = 0; i < IO_SIZE; i++){
+            input[i] = (Math.random() > 0.5 ? 1 : 0);
+        }
+
+        return input;
+    }
+
+    public static double[] generate_expected_output(double[] input){
+        double[] expected_output = new double[IO_SIZE];
+        for (int i = 0; i < IO_SIZE; i++){
+
+            int prev = 0;
+            int next = 0;
+            if (i > 0)
+                prev = (int) input[i - 1];
+
+            if (i < IO_SIZE - 1)
+                next = (int) input[i + 1];
+
+            expected_output[i] = (prev == next ? 0 : 1);
+        }
+        return expected_output;
     }
 }
